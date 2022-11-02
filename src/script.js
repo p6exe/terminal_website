ac46e1be45015ef84c09b51c1736aab61df7f655
p6exe <81422121+p6exe@users.noreply.github.com>
2022-11-01 20:28:28 -0400

    3

--- src/script.js
+++ src/script.js
@@ -1,20 +1,15 @@
 var commands = document.getElementById("commands");
 var user_input = document.getElementById("user_input");
+var terminal_outputs = document.getElementById("terminal_outputs");
 
-temp_btn.addEventListener("click", function(){
-    var newCommand = document.createElement("li");
-    newCommand.innerHTML = commandline.value;
-    commands.appendChild(newCommand);
-    commandline.value = "";
-});
-
-function execute(input) {
-    var newCommand = document.createElement(("li"));
-    newCommand.innerHTML = input;
-    commands.appendChild(newCommand);
+function execute(input){
+    let output;
+    input = input.toLowerCase();
+    output = `<div>testing the input is ${input}</div>`;
+    terminal_outputs.innerHTML = `${terminal_outputs.innerHTML+output}`;
 }
 
-function key(e) {
+function key(e){
     const input = user_input.value;
 
     if(e.key == "Enter"){
@@ -23,7 +18,7 @@ function key(e) {
         return;
     }
 
-    user_input.innerHTML = input + e.key
+    user_input.innerHTML = input + e.key;
 }
 
 document.addEventListener("keypress", key);
